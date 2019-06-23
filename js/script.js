8420ac4b66d3a29b34d0eea0b2e20aa958427d95
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2019-06-23 17:33:20 +0300

    Fixed a problem where the spaces would be ignored.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 // Setting up the variables
 const myElement = document.getElementById("myName");
-const blinker = document.getElementById("blinker");
-const myText = myElement.innerText;
+const myName = "Saleem Abu Shelbayeh";
 
-myElement.innerText = ""; // Clear #myName Element
+// This addes the word in the myNameText letter by letter
+// Using textContent will preserve the spaces in the text.
 
 let i = 0;
-// This addes the word in the myNameText letter by letter
-// It ignores Spacecs (I have no idea why... HELP!!)
-/* 
-    If you Changed the h1 element with the id of MyName in index.html
-    to something with spaces, the spaces will be ignored, and the phrase will be typed as
-    one word.
-    EXAMPLE:
-    Saleem Abu Shelbayeh will be typed as SaleemAbuShelbayeh  
-  */
 const brand = setInterval(() => {
-  if (i < myText.length) {
-    myElement.innerText += myText[i];
+  if (i < myName.length) {
+    myElement.textContent +=  myName[i];
     i++;
   }
 }, 100);
